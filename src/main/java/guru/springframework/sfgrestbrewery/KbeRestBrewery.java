689738fbee7c865e957d393f2a343b3a0ad50dde
sidartaoss <semprebono@gmail.com>
2023-06-14 21:32:52 -0300

    feat: add sfg beers

---
+++ src/main/java/guru/springframework/sfgrestbrewery/KbeRestBrewery.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgrestbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbeRestBrewery {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbeRestBrewery.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/sfgrestbrewery/KbeRestBreweryApplicationTests.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgrestbrewery;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class KbeRestBreweryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
