772f3f522e2354bdc339c77c6e8690c550ca1b92
sidartaoss <semprebono@gmail.com>
2023-06-21 22:24:16 -0300

    chore: update terraform

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -49,7 +49,7 @@ resource "google_container_node_pool" "primary_nodes" {
     }
 
     # preemptible  = true
-    machine_type = "n1-standard-1"
+    machine_type = "n1-standard-2"
     tags         = ["gke-node", "${var.project_id}-gke"]
     metadata = {
       disable-legacy-endpoints = "true"
